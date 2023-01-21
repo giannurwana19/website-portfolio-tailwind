8aa7421181d8b941d8135a9a4f165f0f3a7e60b1
giannurwana19 <giannurwana19@gmail.com>
2023-01-21 13:56:05 +0700

    navbar - membuat navigasi pada lg

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,5 +1,21 @@
+window.onscroll = function () {
+  const header = document.querySelector('header');
+  const fixedNav = header.offsetTop; // posisi header thd posisi atas
+
+  if (window.pageYOffset > fixedNav) {
+    header.classList.add('navbar-fixed');
+  } else {
+    header.classList.remove('navbar-fixed');
+  }
+};
+
 const hamburger = document.getElementById('hamburger');
+const navMenu = document.getElementById('nav-menu');
 
 hamburger.addEventListener('click', function () {
   hamburger.classList.toggle('hamburger-active');
+  navMenu.classList.toggle('hidden');
 });
+
+console.log('offsetTop: ', document.querySelector('header').offsetTop);
+console.log('pageYOffset: ', window.pageYOffset);
